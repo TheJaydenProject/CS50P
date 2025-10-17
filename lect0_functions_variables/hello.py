a8f55336f7d34e21d182442f72e70c8f3fa2861b
Jayden <45730353+TheJaydenProject@users.noreply.github.com>
2025-10-17 12:53:42 +0800

    Lect0_42mins

--- lect0_functions_variables/hello.py
+++ lect0_functions_variables/hello.py
@@ -1,3 +1,33 @@
-print("Hello, world!")
-name = input("whats your name? ")
-print("Hello, " + name)
+# Say hello world
+print('Hello, world!')
+
+# Ask user for their name
+name = input('whats your name? ') # Input function expects a string
+
+# Say hello to the user
+print('Hello, ' + name)
+
+# Another way to format strings
+print('Hello,', name)
+print(f'Hello, {name}')
+
+# Print Doc
+# print(*objects, sep=' ', end='\n', file=None, flush=False)
+# *objects: Any number of objects can be printed.
+# sep: String inserted between values, default is a space.
+# end: String appended after the last value, default is a newline (\n).
+# file: An object with a write(string) method; defaults to the current sys.stdout.
+# flush: A boolean, specifying whether to flush the output buffer.
+
+# Solve Name being printed on a new line
+print('Hello, ')
+print(name)
+
+# Solution
+print('Hello,', sep=' ', end='')
+print(name)
+
+# Putting Quotes in quotes of Print function
+# Use different quotes to seperate
+print("Hello, 'friend'")
+
