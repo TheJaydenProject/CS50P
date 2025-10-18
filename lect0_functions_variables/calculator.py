8432897a1b72753132854398a02e7fdf3a207fe1
Jayden <45730353+TheJaydenProject@users.noreply.github.com>
2025-10-18 17:46:40 +0800

    Lect0_1h11mins

---
+++ lect0_functions_variables/calculator.py
@@ -0,0 +1,12 @@
+x = input('What\'s x? ')
+y = input('What\'s y? ')
+
+z = int(x) + int(y)
+
+print(z)
+
+# nest functions
+x = int(input('What\'s x? '))
+y = int(input('What\'s y? '))
+
+print(x + y)
